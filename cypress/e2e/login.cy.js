26e9549f138949f24401bc8f2bbbdb83a77a306f
Dragan Mijatovic <mijatovicd77@gmail.com>
2023-02-09 14:59:35 +0100

    cypress/prva-grana

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,9 +1,9 @@
 describe('login test', () => {
-    it('login with valid credentials', () => {
+    it.only('login with valid credentials', () => {
         cy.visit("/");
         cy.get(".nav-link").eq(1).click();
-        cy.get("#email").type("dragannmijatovic77@outlook.com");
-        cy.get("#password").type("alfaromeo159");
+        cy.get("#email").type("vivify11@test.com");
+        cy.get("#password").type("dragan123");
         cy.get("button").click();
         cy.get(".nav-link").should("have.length", 4);
         cy.url().should("not.contain", "/login");
@@ -12,8 +12,8 @@ describe('login test', () => {
     it("login with invalid email", () => {
         cy.visit("/");
         cy.get(".nav-link").eq(1).click();
-        cy.get("#email").type("dragannmijatovic77@");
-        cy.get("#password").type("alfaromeo159");
+        cy.get("#email").type("vivify11@");
+        cy.get("#password").type("dragan123");
         cy.get("button").click();
     })
 });
